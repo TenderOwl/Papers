169893b783385093e1afd5270cc607989b9fcd27
Andrey Maksimov <meamka@ya.ru>
2022-12-19 15:44:30 +0300

    Add async methods to BooksService

--- lib/src/services/books_service.dart
+++ lib/src/services/books_service.dart
@@ -10,8 +10,16 @@ class BooksService {
     return isar.books.where().findAllSync();
   }
 
+  Future<List<Book>> getAll() {
+    return isar.books.where().findAll();
+  }
+
   Book? getSync(int bookId) {
     return isar.books.getSync(bookId);
+  }
+
+  Future<Book?> get(int bookId) {
+    return isar.books.get(bookId);
   }
 
   int putSync(Book book) {
@@ -20,12 +28,22 @@ class BooksService {
     return isar.writeTxnSync(() => isar.books.putSync(book));
   }
 
+  Future<int> put(Book book) {
+    book.createdAt = DateTime.now();
+    book.updatedAt = DateTime.now();
+    return isar.writeTxn(() async => await isar.books.put(book));
+  }
+
   void updateSync(Book book) {
     book.updatedAt = DateTime.now();
     return isar.writeTxnSync(() => isar.books.putSync(book));
   }
 
   bool removeSync(int bookId) {
-    return isar.books.deleteSync(bookId);
+    return isar.writeTxnSync(() => isar.books.deleteSync(bookId));
+  }
+
+  Future<bool> remove(int bookId) {
+    return isar.writeTxn(() async => await isar.books.delete(bookId));
   }
 }
